fe4a5efd32af10ea097e63b1940aee6aa01f4fa9
Martialcart <janolav91@hotmail.com>
2021-11-10 12:02:02 +0100

    cleaned out bugs bmi

--- js/bmiv2.js
+++ js/bmiv2.js
@@ -1,4 +1,7 @@
 const n_decimals = 2;
+const bmi_low = 18;
+const bmi_mid = 25;
+const bmi_high = 30
 
 /*connects gui and action*/
 function handle_input() {
@@ -9,13 +12,13 @@ function handle_input() {
     const r18 = document.getElementById("r18");
     const r25 = document.getElementById("r25");
     const r30 = document.getElementById("r30");
-    r18.innerHTML = kg(18, to_m(cm_inn.value));
-    r25.innerHTML = kg(25, to_m(cm_inn.value));
-    r30.innerHTML = kg(30, to_m(cm_inn.value));
+    r18.innerHTML = kg(bmi_low, to_m(cm_inn.value));
+    r25.innerHTML = kg(bmi_mid, to_m(cm_inn.value));
+    r30.innerHTML = kg(bmi_high, to_m(cm_inn.value));
     bmi_result = bmi(kg_inn.value, to_m(cm_inn.value));
     console.log(bmi_result);
     r_bmi.innerHTML = bmi_result;
-    document.getElementById("bmi").style.marginLeft = relative_dist(30, bmi_result);
+    document.getElementById("bmi").style.marginLeft = bmi_screen_scale(bmi_result);
 }
 
 /*returns kg, from bmi and height*/
@@ -33,7 +36,16 @@ function to_m(cm) {
     return cm / 100;
 }
 
-/*percantage of screen*/
-function relative_dist(scale, dist) {
-    return (10 + (dist / scale) * 80) + "%";
+/*returns the % of where the bmi result is placed on the screen*/
+function bmi_screen_scale(bmi) {
+    const scale = bmi_high - bmi_low;
+    const range = bmi - bmi_low;
+    let result = 0;
+
+    if (bmi < bmi_low) result = "0%";
+    else if (bmi < bmi_mid) result = (range / scale) * 65 + "%"; //number adjust for margin inconsistensies
+    else if (bmi_high < bmi) result = "80%";
+    else result = (range / scale) * 80 + "%"; //number adjust for margin inconsistensies
+    
+    return result;
 }
