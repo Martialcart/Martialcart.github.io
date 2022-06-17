737b43aaea8b4c9edf9ca846225365696a5e3bb2
Jan Olav Berg <janolav91@hotmail.com>
2022-06-17 11:30:46 +0200

    must run draw() manually in power for graphs to be read and  show

---
+++ js/power.js
@@ -0,0 +1,35 @@
+let data = {};
+
+async function loadprices() {
+    const response = await fetch("./json/power/2022-06-15.json");
+    const json = await response.json();
+    data = json;
+}
+
+
+function draw(){
+    loadprices();
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'],
+            datasets: [
+                {
+                label: 'bergen',
+                data: data.Bergen,
+                borderColor: 'blue',
+                fill: false,
+                steppedLine: true
+                },
+                {
+                label: 'trondheim',
+                data: data.Trondheim,
+                borderColor: 'red',
+                fill: false,
+                steppedLine: true,
+                }
+        ],  
+        },
+    });
+}
