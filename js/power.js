bd4305f30ba5712de373c4baa853c9825778c506
Jan_Olav_Berg <janolav91@hotmail.com>
2022-08-13 12:49:43 +0200

    added instruction for updating power_chart

--- js/power.js
+++ js/power.js
@@ -1,5 +1,11 @@
 let prices_today = {};
 let prices_tomorrow = {};
+let myChart = {};
+/*
+update mychart by finding path in console
+mychart.update()
+
+*/
 
 
 let date_gen = new Date();
@@ -42,7 +48,7 @@ async function get_power_price(date) {
 async function draw(){
     await loadprices();
     let ctx = document.getElementById('myChart').getContext('2d');
-    const myChart = new Chart(ctx, {
+    myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: [date_today + ' 00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00',
